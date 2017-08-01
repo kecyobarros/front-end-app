dea6b3c90db80ee941d1a5f3a4fe30a659635613
Kecyo Barros <kecyobarros@gmail.com>
2017-07-31 22:09:52 -0300

    Colocando paginação

--- front-end-app/client/lista/lista.js
+++ front-end-app/client/lista/lista.js
@@ -1,14 +1,64 @@
 Template.lista.helpers({
+
   usuarios : function(){
-    Meteor.call('userFindPage', 1);
-    return UserCollection.find({});
+    var pagina = 0;
+    Session.set("pagina", pagina);
+    Meteor.call('userFindPage', pagina);
+    var json = UserCollection.find({});
+    return json;
   }
 });
+
+getPagina = function(){
+  return Session.get("pagina");
+}
+
+setPagina = function(pagina){
+   Session.set("pagina", pagina);
+}
+
+proximaPagina = function(){
+  var pagina = getPagina();
+  pagina++;
+  setPagina(pagina);
+
+  if($( "#liPrevious, #liPreviousFooter" ).hasClass("disabled")){
+    $( "#liPrevious, #liPreviousFooter" ).removeClass("disabled");
+  }
+
+  return pagina;
+}
+
+anteriorPagina = function(){
+  var pagina = getPagina();
+  pagina--;
+  setPagina(pagina);
+  if (pagina <=0){
+    $( "#liPrevious, #liPreviousFooter" ).addClass( "disabled" );
+  }
+  return pagina;
+}
 
 Template.lista.events({
     "click [data-action='detalhe']": function (event, template) {
         event.preventDefault();
         var user = this;
         Meteor.call('deviceFindByUserId', user.id);
+    },
+
+    "click [data-action='previous']": function (event, template) {
+        event.preventDefault();
+        var pagina = getPagina();
+
+        if (pagina > 0){
+          pagina = anteriorPagina();
+          Meteor.call('userFindPage', pagina);
+        }
+    },
+
+    "click [data-action='next']": function (event, template) {
+        event.preventDefault();
+        var pagina = proximaPagina();
+        Meteor.call('userFindPage', pagina);
     }
 });
